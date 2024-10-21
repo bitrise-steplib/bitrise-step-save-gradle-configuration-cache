e3e829938ac6288c88b5aaa2f4e72ea4cb33492c
zsolt-marta-bitrise <zsolt.marta@bitrise.io>
2024-10-21 09:31:58 +0200

    feat: ACI-2928 Create save Gradle step (#1)

---
+++ step.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+# 'read' has to be before 'set -e'
+read -r -d '' UNAVAILABLE_MESSAGE << EOF_MSG
+Bitrise Build Cache is not activated in this build.
+
+It seems you don't have an activate Bitrise Build Cache Trial or Subscription for the current workspace yet.
+
+You can activate a Trial at [app.bitrise.io/build-cache](https://app.bitrise.io/build-cache),
+or contact us at [support@bitrise.io](mailto:support@bitrise.io) to activate it.
+EOF_MSG
+
+set -eo pipefail
+
+echo "Checking whether Bitrise Build Cache is activated for this workspace ..."
+if [ "$BITRISEIO_BUILD_CACHE_ENABLED" != "true" ]; then
+  printf "\n%s\n" "$UNAVAILABLE_MESSAGE"
+  set -x
+  bitrise plugin install https://github.com/bitrise-io/bitrise-plugins-annotations.git
+  bitrise :annotations annotate "$UNAVAILABLE_MESSAGE" --style error || {
+    echo "Failed to create annotation"
+    exit 3
+  }
+  exit 2
+fi
+echo "Bitrise Build Cache is activated in this workspace"
+
+set -x
+
+# download the Bitrise Build Cache CLI
+export BITRISE_BUILD_CACHE_CLI_VERSION=${BITRISE_BUILD_CACHE_CLI_VERSION:="v0.15.0"}
+curl --retry 5 -sSfL 'https://raw.githubusercontent.com/bitrise-io/bitrise-build-cache-cli/main/install/installer.sh' | sh -s -- -b /tmp/bin -d $BITRISE_BUILD_CACHE_CLI_VERSION
+
+cmd="/tmp/bin/bitrise-build-cache save-gradle-configuration-cache --config-cache-dir $config_cache_dir"
+
+if [ "$verbose" = "true" ]; then
+  cmd="$cmd -d"
+fi
+
+if [ -n "$key_override" ]; then
+  cmd="$cmd --key $key_override"
+fi
+
+$cmd
